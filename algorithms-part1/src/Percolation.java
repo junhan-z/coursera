da7dd5d142f964d0b47d457965695a381ce7ae65
Junhan <junhanoct@gmail.com>
2016-10-19 00:08:25 -0700

    use 1D array

--- algorithms-part1/src/Percolation.java
+++ algorithms-part1/src/Percolation.java
@@ -3,111 +3,99 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private int top = 0;
     private int bottom;
-    private Integer[][] matrix;
     private int size;
     private WeightedQuickUnionUF uf;
+    private byte[] site;
 
     public Percolation(int n) {
         if (n < 0) {
             throw new IllegalArgumentException();
         }
+        // assign top with 0, bottom with n^2 + 1, n^2 + 2 in total
         top = 0;
         bottom = n * n + 1;
-
-        // initialization
-        // 0, n-1
-        matrix = new Integer[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = null;
-            }
-        }
         size = n;
-
         uf = new WeightedQuickUnionUF(n * n + 2);
+        site = new byte[n * n];
     }
 
     public void open(int row, int col) {
-        row = row - 1;
-        col = col - 1;
+        if (isOpen(row, col))
+            return;
 
-        if (row < 0 || row > size - 1 || col < 0 || col > size - 1) {
-            throw new IndexOutOfBoundsException();
-        }
-
-        if (matrix[row][col] == null) {
-            matrix[row][col] = row * size + col + 1;
-        }
-        connectNeighbor(row, col);
+        site[convertIndex(row, col)] = 1;
+        connectNeighbors(row, col);
     }
 
     // row and col are based on [0, n-1]
-    private void connectNeighbor(int row, int col) {
+    private void connectNeighbors(int row, int col) {
+        int curIndex = convertIndex(row, col);
         // up
-        if (row > 0  && matrix[row-1][col] != null) {
-            uf.union(matrix[row][col], matrix[row-1][col]);
-        } else if (row == 0) {
-            uf.union(matrix[row][col], top);
+        if (row > 1 && isOpen(row - 1, col)) {
+            uf.union(curIndex, convertIndex(row - 1, col));
+        } else if (row == 1) {
+            uf.union(curIndex, top);
         }
 
         // down
-        if (row + 1 < size && matrix[row+1][col] != null) {
-            uf.union(matrix[row][col], matrix[row+1][col]);
-        } else if (row == size -1) {
-            uf.union(matrix[row][col], bottom);
+        if (row < size && isOpen(row + 1, col)) {
+            uf.union(curIndex, convertIndex(row + 1, col));
+        } else if (row == size) {
+            uf.union(curIndex, bottom);
         }
         // left
-        if (col > 0 && matrix[row][col-1] != null) {
-            uf.union(matrix[row][col], matrix[row][col-1]);
+        if (col > 1 && isOpen(row, col - 1)) {
+            uf.union(curIndex, convertIndex(row, col - 1));
         }
         // right
-        if (col + 1 < size && matrix[row][col+1] != null) {
-            uf.union(matrix[row][col], matrix[row][col+1]);
+        if (col < size && isOpen(row, col + 1)) {
+            uf.union(curIndex, convertIndex(row, col+1));
         }
     }
 
     public boolean isOpen(int row, int col) {
-        row -= 1;
-        col -= 1;
-        if (row < 0 || row > size - 1 || col < 0 || col > size - 1) {
-            throw new IndexOutOfBoundsException();
-        }
-        return matrix[row][col] != null;
+        isInBound(row, col);
+        return site[convertIndex(row, col)] != 0;
     }
 
     public boolean isFull(int row, int col) {
         // virtual top and bottom are connected and
         // this point is connected to it
-        row -= 1;
-        col -= 1;
-        if (row < 0 || row > size - 1 || col < 0 || col > size - 1) {
-            throw new IndexOutOfBoundsException();
-        }
-        return uf.connected(top, bottom) && uf.connected(matrix[row][col], top);
+        if (!isOpen(row, col)) return false;
+
+        int index = convertIndex(row, col);
+        if (site[index] == 2) return true;
+        boolean result = uf.connected(top, bottom) && uf.connected(index, top);
+        if (result) site[index] = 2;
+
+        return result;
     }
 
     public boolean percolates() {
         return uf.connected(top, bottom);
     }
 
+    private boolean isInBound(int row, int col) {
+        if (row < 1 || row > size || col < 1 || col > size) {
+            throw new IndexOutOfBoundsException(String.format("row %d col %d", row, col));
+        }
+        return true;
+    }
+
+    private int convertIndex(int row, int col) {
+        return (row - 1) * size + col - 1;
+    }
+
     public void display() {
-        for (int i = 0; i < size; i++) {
+        for (int i = 1; i <= size; i++) {
             String content = "";
-            for (int j = 0; j < size; j++) {
-                content += matrix[i][j] + " ";
+            for (int j = 1; j <= size; j++) {
+                content += site[convertIndex(i, j)] + " ";
             }
             System.out.println(content);
         }
     }
 
-    public boolean connected(int row1, int col1, int row2, int col2) {
-        row1 -= 1;
-        col1 -= 1;
-        row2 -= 1;
-        col2 -= 1;
-
-        return uf.connected(row1 * size + col1 + 1, row2 * size + col2 + 1);
-    }
     public static void main(String[] args) {
         Percolation p = new Percolation(10);
         p.display();
